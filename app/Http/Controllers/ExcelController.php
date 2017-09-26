ee5a39ae4589bbffbc4773637fbe92e981d94214
Ricky <isu10304079a@cloud.isu.edu.tw>
2017-09-26 21:29:50 +0800

    新增匯入 Excel 檔邏輯 import() @ExcelController

--- app/Http/Controllers/ExcelController.php
+++ app/Http/Controllers/ExcelController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Excel;
+use Faker\Factory;
 use Illuminate\Http\Request;
 
 class ExcelController extends Controller
@@ -38,5 +39,35 @@ class ExcelController extends Controller
                 });
             });
         })->export('xlsx');
+    }
+
+    /**
+     * 匯入 Excel 檔
+     *
+     * @param Request $request
+     */
+    public function import(Request $request) {
+        //取得檔案副檔名
+        $extension = pathinfo($_FILES['import_file']['name'], PATHINFO_EXTENSION);
+
+        //判斷是否有來自於 import_file 的檔案，並只允許上傳 Excel 檔
+        if ($request->hasFile('import_file') && ($extension === "xlsx" || $extension === "xls")) {
+            $filePath = $request->file('import_file')->getRealPath();
+
+            Excel::load($filePath, function ($reader) {
+                $data = $reader->get(array('姓名', '信箱'));
+                $faker = Factory::create();
+
+                foreach ($data as $rows) {
+                    $this->user->create([
+                        'name'     => $rows->get("姓名"),
+                        'email'    => $rows->get("信箱"),
+                        'password' => $faker->bothify('##?##?##?')
+                    ]);
+                }
+            });
+        } else {
+            abort(404);
+        }
     }
 }
